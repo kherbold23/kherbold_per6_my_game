2ede4dea1b5f675f61306eec35789555e650d8a4
kherbold23 <k.herbold23@bcp.org>
2023-03-31 10:50:34 -0700

    platforms

--- main.py
+++ main.py
@@ -5,8 +5,13 @@
 # Create libraries
 
 
-# This file was created by: Chris Cozort
-# Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
+'''
+My goals are:
+
+
+
+
+'''
 
 # import libs
 import pygame as pg
@@ -25,26 +30,37 @@ img_folder = os.path.join(game_folder, "img")
 
 class Game:
     def __init__(self):
-            # init game window
-            pg.init()
-            pg.mixer.init()
-            self.screen = pg.display.set_mode((WIDTH,HEIGHT))
-            pg.display.set_caption("my game")
-            self.clock = pg.time.Clock()
-            self.running = True
-
+        # init game window etc.
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("my game")
+        self.clock = pg.time.Clock()
+        self.running = True
+        print(self.screen)
     def new(self):
-        # start new game
+        # starting a new game
         self.score = 0
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
         self.enemies = pg.sprite.Group()
         self.player = Player(self)
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        # self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
         self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
         for i in range(0,10):
             m = Mob(20,20,(0,255,0))
             self.all_sprites.add(m)
             self.enemies.add(m)
+        self.run()
     def run(self):
         self.playing = True
         while self.playing:
@@ -52,21 +68,35 @@ class Game:
             self.events()
             self.update()
             self.draw()
+    
     def events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_SPACE:
-                self.player.jump()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
     def update(self):
         self.all_sprites.update()
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+
     def draw(self):
         self.screen.fill(BLUE)
-        pg.display.flip()
         self.all_sprites.draw(self.screen)
+        # is this a method or a function?
+        pg.display.flip()
     def draw_text(self, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
@@ -78,8 +108,11 @@ class Game:
         x,y = pg.mouse.get_pos()
         return (x,y)
 
+# instantiate the game class...
 g = Game()
+
+# kick off the game loop
 while g.running:
-        g.new()
-        
+    g.new()
+
 pg.quit()
--- sprites.py
+++ sprites.py
@@ -4,64 +4,76 @@ from pygame.sprite import Sprite
 
 from settings import *
 
-vec = pg.math.Vector2
-
 from random import randint
 
+vec = pg.math.Vector2
 # player class
 
 class Player(Sprite):
     def __init__(self, game):
         Sprite.__init__(self)
+        # these are the properties
         self.game = game
         self.image = pg.Surface((50,50))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.pos = 0,0
+        self.pos = vec(WIDTH/2, HEIGHT/2)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def input(self):
         keystate = pg.key.get_pressed()
-
-        if keystate[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
+        # if keystate[pg.K_w]:
+        #     self.acc.y = -PLAYER_ACC
         if keystate[pg.K_a]:
             self.acc.x = -PLAYER_ACC
-        if keystate[pg.K_s]:
-            self.acc.y = PLAYER_ACC
+        # if keystate[pg.K_s]:
+        #     self.acc.y = PLAYER_ACC
         if keystate[pg.K_d]:
             self.acc.x = PLAYER_ACC
-    def jump(self):
-        hits = pg.sprite.spritecollide(self, self.game.platforms,False)
+        # if keystate[pg.K_p]:
+        #     if PAUSED == False:
+        #         PAUSED = True
+        #         print(PAUSED)
+        #     else:
+        #         PAUSED = False
+        #         print(PAUSED)
     # ...
+    def jump(self):
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        # if hits:
+        self.vel.y = -PLAYER_JUMP
+    
     def inbounds(self):
         if self.rect.x > WIDTH - 50:
             self.pos.x = WIDTH - 25
             self.vel.x = 0
-            # print("i am off the right side of the screen...")
+            print("i am off the right side of the screen...")
         if self.rect.x < 0:
-            self.pos.x = WIDTH - 775
+            self.pos.x = 25
             self.vel.x = 0
-            # print("i am off the left side of the screen...")
+            print("i am off the left side of the screen...")
         if self.rect.y > HEIGHT:
-            self.pos.y = HEIGHT - 25
-            self.vel.y = 0
-            # print("i am off the bottom of the screen")
+            print("i am off the bottom of the screen")
         if self.rect.y < 0:
-            self.pos.y = HEIGHT - 575
-            self.vel.y = 0
-            # print("i am off the top of the screen...")
-
+            print("i am off the top of the screen...")
+    def mob_collide(self):
+            hits = pg.sprite.spritecollide(self, self.game.enemies, True)
+            if hits:
+                print("you collided with an enemy...")
+                self.game.score += 1
+                print(SCORE)
     def update(self):
-        self.inbounds()
-        self.acc = self.vel * PLAYER_FRICTION
+        self.acc = vec(0, PLAYER_GRAV)
+        self.acc.x = self.vel.x * PLAYER_FRICTION
         self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-        self.rect.center = self.pos
+        self.rect.midbottom = self.pos
 
 class Mob(Sprite):
     def __init__(self,width,height, color):
@@ -93,7 +105,22 @@ class Mob(Sprite):
             # self.acc = self.vel * -self.cofric
     def update(self):
         self.inbounds()
-        self.rect.x += self.vel.x
-        self.rect.center = self.pos
+        # self.pos.x += self.vel.x
+        # self.pos.y += self.vel.y
         self.pos += self.vel
         self.rect.center = self.pos
+
+# create a new platform class...
+
+class Platform(Sprite):
+    def __init__(self, x, y, width, height, color, variant):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.variant = variant
